36119161ac657a0c9f20fe86a78441e462bf1bad
Peron152 <peron.anderson@escola.pr.gov.br>
2025-08-11 12:03:36 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,22 +1,22 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Biologia',
+    'Qual é o nome do processo pelo qual as plantas produzem seu próprio alimento, usando luz solar, água e gás carbônico?',
+    'Resposta: Fotossíntese'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
+    'Biologia',
+    'Qual é a parte de uma célula vegetal que lhe dá sua estrutura rígida e formato?',
     'A capital da França é Paris'
 )
 
 criaCartao(
-    'Programação',
+    'Biologia',
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
     criaCartao(
-    'Lingua inglesa',
+    'Biologia',
     'Como se diz OI em inglês?',
     'Oi em ingles é HI (RAI)'
 )
